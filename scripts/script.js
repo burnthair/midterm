db0f8eab5d6dd0ce190bed83b8af34ae66cc1c67
trisafotp <trisafotp@gmail.com>
2017-07-13 12:51:08 -0400

    finally stopped panicking (thank god). mods to js aimed at populating modal with inputs (putting cart before the horse). additions to html and css aimed at creating modal in the first place.

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,14 @@ $('div.seat').click(function(){
 $('div.seat').click(function (){
   $('#ui').removeClass('hidden')
 });
+
+//Loading user inputs into modal
+$('#submit').click(function () {
+  var firstName = document.getElementById('fname');
+  var lastName = document.getElementById('lname');
+  var email = document.getElementById('email');
+  var phone = document.getElementById('phone');
+
+console.log(firstName);
+
+})
