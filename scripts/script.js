4f3034b4419d4d62581c3dc943d48135985bd875
trisafotp <trisafotp@gmail.com>
2017-07-13 14:40:08 -0400

    paltry attempts to make modal appear.

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,18 @@
-console.log('Have fun!');
-
 //Upon click, select seat
 $('div.seat').click(function(){
   $(this).toggleClass('selected-seat')
 })
-
-
 //Reveal UI upon seat selection
 $('div.seat').click(function (){
   $('#ui').removeClass('hidden')
 })
 //Clicking submit button opens confirmation modal
 $('#submit').click(function() {
-  //$('#modal-container').fadeIn('fast')
-  console.log('I work!')
+  $('#modal-container').removeClass('hidden')
 })
+/*
 //Clicking X button on modal closes confirmation modal
 $('#close').click(function() {
   $('#modal-container').fadeOut('slow')
 })
+*/
