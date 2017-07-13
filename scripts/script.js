b475e2b3a8cdbb885c5c49b4c2039b6cd9b07a5d
Michael Edwards <edwardsmichaelv@gmail.com>
2017-07-13 13:25:02 -0400

    Switching branches

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
   // Upon clicking on the curtain, the container holding the seats transitions down onto the screen. Upon moving into place, display header
-  $('.seat').on('click', function(){
+  $(window).on('click', function(){
     $('.container').addClass("container-is-active");
     setTimeout(function() {
       $('#header').fadeTo(2000, 1.0);
