60f8595255ad8db073a585df7d3e958cfaea9209
trisafotp <trisafotp@gmail.com>
2017-07-13 10:53:01 -0400

    upon click, class selected-seat is now applied. still unable to make ui disappear when no seats are selected.

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,14 @@
 console.log('Have fun!');
 
+//Upon click, select seat
+$('div.seat').click(function(){
+  $(this).toggleClass('selected-seat')
+})
+
+
 //Reveal UI upon seat selection
 $('div.seat').click(function (){
   $('#ui').removeClass('hidden')
 });
+
+//Hide UI when no seats are selected
