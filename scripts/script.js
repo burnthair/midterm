c1e931443e9cff440503ad8cf2ef8295c66e2758
Michael Edwards <edwardsmichaelv@gmail.com>
2017-07-13 11:36:41 -0400

    Added functionality for seat container to transition down into viewport, and header fades in when seat container is in place.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
-console.log('Have fun!');
+$(document).ready(function(){
+
+  // Upon clicking on the curtain, the container holding the seats transitions down onto the screen. Upon moving into place, display header
+  $('.seat').on('click', function(){
+    $('.container').addClass("container-is-active");
+    setTimeout(function() {
+      $('#header').fadeTo(2000, 1.0);
+    }, 2000);
+  });
+
+});
