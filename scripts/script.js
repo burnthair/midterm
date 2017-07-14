74ad936cd32c9df78a3bbe42a6cd55fbbb2f9695
trisafotp <trisafotp@gmail.com>
2017-07-14 13:50:18 -0400

    user reservation information appears upon all reserved seats with mouse hover. fully functional.

--- scripts/script.js
+++ scripts/script.js
@@ -11,48 +11,61 @@ $(document).ready(function(){
     }, 2000);
   });
 
+
+  //Upon clicking a seat, select seat
+  $('div.seat').click(function(){
+    $(this).toggleClass('selected-seat')
+  })
+
+  //Reveal UI upon seat selection
+  $('div.seat').click(function (){
+    $('#user-inputs').removeClass('hidden')
+  });
+
+  // Upon clicking the Submit button, a modal appears
+  // Upon clicking the Submit button, the user input is placed into an object
+  // The user input is displayed in the modal
+  $('#submit').click(function(){
+    $('.modal-container').removeClass('hidden');
+    var reservedSeats = document.getElementsByClassName('reserved-seat')
+    var chosenSeats = document.getElementsByClassName("selected-seat");
+    var patron = {
+      firstName : document.getElementById('fname').value,
+      lastName : document.getElementById('lname').value,
+      phoneNumber : document.getElementById('phone').value,
+      email : document.getElementById('email').value
+    }
+    $('.insert-first-name').append(patron.firstName);
+    $('#insert-last-name').append(patron.lastName);
+    $('#insert-phone-number').append(patron.phoneNumber);
+    $('#insert-email').append(patron.email);
+    for (var i = 0; i < chosenSeats.length; i++) {
+      $('#insert-seat-ids').append(chosenSeats[i].id + ' ');
+      $(chosenSeats[i]).addClass('reserved-seat');
+    }
+    for (var i=0; i<reservedSeats.length; i++) {
+      $(reservedSeats[i]).html('<span class="reserved-by hide">'+patron.firstName+ ' ' +patron.lastName+ '</span>');
+    }
+    $('.selected-seat').removeClass('selected-seat');
+    chosenSeats.length = 0;
+  });
+
+  $('.row .seat ').on('mouseenter', function(e) {
+    e.target.children[0].className = 'reserved-by';
+  }).on('mouseleave', function(e) {
+    e.target.children[0].className = 'reserved-by hide';
+  });
+
+
+
+  $('.modal-container').click(function(){
+    $('.modal-container').addClass('hidden');
+    $('.insert-first-name').html('<span> </span>');
+    $('#insert-last-name').html('<span> </span>');
+    $('#insert-phone-number').html('<span> </span>');
+    $('#insert-email').html('<span> </span>');
+    $('#insert-seat-ids').html('<span> </span>');
+    $('form')[0].reset();
+  })
+
 });
-
-//Upon clicking a seat, select seat
-$('div.seat').click(function(){
-  $(this).toggleClass('selected-seat')
-})
-
-//Reveal UI upon seat selection
-$('div.seat').click(function (){
-  $('#user-inputs').removeClass('hidden')
-});
-
-// Upon clicking the Submit button, a modal appears
-// Upon clicking the Submit button, the user input is placed into an object
-// The user input is displayed in the modal
-$('#submit').click(function(){
-  $('.modal-container').removeClass('hidden');
-  var chosenSeats = document.getElementsByClassName("selected-seat");
-  var patron = {
-    firstName : document.getElementById('fname').value,
-    lastName : document.getElementById('lname').value,
-    phoneNumber : document.getElementById('phone').value,
-    email : document.getElementById('email').value
-  }
-  $('.insert-first-name').append(patron.firstName);
-  $('#insert-last-name').append(patron.lastName);
-  $('#insert-phone-number').append(patron.phoneNumber);
-  $('#insert-email').append(patron.email);
-  for (var i = 0; i < chosenSeats.length; i++) {
-    $('#insert-seat-ids').append(chosenSeats[i].id + ' ');
-    $(chosenSeats[i]).addClass('reserved-seat');
-  }
-  $('.selected-seat').removeClass('selected-seat');
-  chosenSeats.length = 0;
-});
-
-$('.modal-container').click(function(){
-  $('.modal-container').addClass('hidden');
-  $('.insert-first-name').html('<span> </span>');
-  $('#insert-last-name').html('<span> </span>');
-  $('#insert-phone-number').html('<span> </span>');
-  $('#insert-email').html('<span> </span>');
-  $('#insert-seat-ids').html('<span> </span>');
-  $('form')[0].reset();
-})
